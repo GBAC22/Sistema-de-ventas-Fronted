c8ac18abfd00b1be12b59e5afffc32ff31a0137d
Gary Brian <86137843+GBAC22@users.noreply.github.com>
2025-08-08 11:42:52 -0400

    Fronted Completo

---
+++ appflutter/lib/config.dart
@@ -0,0 +1,9 @@
+class Config {
+  static const String appName = "Empresa XYZ";
+  static const String apiURL = '10.0.2.2:8000';
+  static const productosAPI = "api/producto";
+  static const clientesAPI = "api/cliente";
+  static const loginAPI = "api/login/";
+  static const obtenertokenAPI = "api/api-token-auth/";
+}
+
